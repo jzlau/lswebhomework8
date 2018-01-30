a54f0885ffa4debbd29878a9cbd91686bb3ecf3c
unknown <jzlau86@gmail.com>
2018-01-30 12:37:39 -0500

    completed

--- callbacks.js
+++ callbacks.js
@@ -24,11 +24,19 @@ firstItem(foods, function(firstItem) {
   console.log('The first item is ' + firstItem);
 });
 
+function firstItem(foods,cb){
+  cb(foods[0]);
+};
+
 // Write a function called getLength that passes the length of the array into the callback
 
 getLength(foods, function(length) {
   console.log('The length of the array is ' + length);
 });
+
+function getLength(foods,cb){
+  cb(foods.length);
+};
 
 // Write a function called last which passes the last item of the array into the callback
 
@@ -36,17 +44,29 @@ last(foods, function(lastItem) {
   console.log('The last item in the array is ' + lastItem);
 });
 
+function last(foods,cb){
+  cb(foods[foods.length-1]);
+}
+
 // Write a function called sumNums that adds two numbers and passes the result to the callback
 
 sumNums(5, 10, function(sum) {
   console.log('The sum is ' + sum);
 });
 
+function sumNums(num1,num2,cb){
+  cb(num1 +num2);
+};
+
 // Write a function called multiplyNums that adds two numbers and passes the result to the callback
 
 multiplyNums(5, 10, function(product) {
   console.log('The product is ' + product);
 });
+
+function multipleNums (num1,num2,cb){
+  cb(num1 * num2);
+};
 
 // Write a function called contains that checks if an item is present inside of the given array.
 // Pass true to the callback if it is, otherwise pass false
@@ -55,6 +75,13 @@ contains(foods, 'ribeye', function(result) {
   console.log(result ? 'ribeye is in the array' : 'ribeye is not in the array');
 });
 
+function contains(foods, item1, cb){
+  if (foods.indexOf(item1) > -1){
+    return cb(foods);
+  } else {
+    return false;
+  }
+
 // Write a function called removeDuplicates that removes all duplicate values from the given array.
 // Pass the array to the callback function.  Do not mutate the original array.
 
@@ -62,8 +89,26 @@ removeDuplicates(foods, function(uniqueFoods) {
   console.log('foods with duplicates removed: ' + uniqueFoods);
 });
 
+function removeDuplicates (foods, cb){
+  var newArr =[];
+  for (var i = 0; i < foods.length; i++){
+    if(newArr.indexOf(foods[i])=== -1){
+      newArr.push(foods[i])
+    }
+  }
+  return cb(newArr);
+}
+
+
 // Write a function called forEach that iterates over the provided array and passes the value and index into the callback.
 
 forEach(foods, function(value, index) {
   console.log(value + ' is at index ' + index);
 });
+
+function forEach (foods,cb){
+  for (var i = 0; i< foods.length; i++){
+    var value = foods[i];
+    cb(value, i);
+  }
+}
